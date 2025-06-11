d2e7e57dd30bc65d97de5c6683309c385776ba90
Lucasfi <lukafigueroa64@gmail.com>
2025-06-11 16:40:31 -0300

    agregamos una imagen de fondo y la adaptamos responsiva, tambien agregamos bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import MensajeConProps from './components/MensajeConProps'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
   const Mensaje = 'My Friend';
 
   return (
-    <main>
+    <main className='container my-5 text-center text-white'>
       <MensajeConProps mensajeConProps={Mensaje} />
     </main>
   )
