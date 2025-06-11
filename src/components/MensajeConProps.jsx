bbab8edaa2dcbafe7b5ff20d40af75ce3e890321
Lucasfi <lukafigueroa64@gmail.com>
2025-06-11 16:11:24 -0300

    creamos el componente icluyendo funcion y exportamos con su parametro

---
+++ src/components/MensajeConProps.jsx
@@ -0,0 +1,10 @@
+const MensajeConProps = (mensajeConProps) => {
+  return (
+    <div>
+      <h2>¡¡Hello {props.mensajeConProps}!!</h2>
+    </div> 
+  );
+    
+};
+
+export default MensajeConProps;
